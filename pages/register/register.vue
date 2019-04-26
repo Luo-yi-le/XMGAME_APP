5ace8e4bc06d00acc3c337af938fa57e30fd458a
周少鸿 <43126668+xihehua@users.noreply.github.com>
2019-04-26 13:40:48 +0800

    更新注册
    更新注册

--- pages/register/register.vue
+++ pages/register/register.vue
@@ -15,7 +15,7 @@
 				<table>
 					<tr>
 						<th><text>密码：</text></th>
-						<th><input type="password" v-model="user.UserPassWord"  maxlength="9" placeholder="密码" /></th>
+						<th><input type="password"  class="Register_input" v-model="user.UserPassWord"  maxlength="9" placeholder="密码" /></th>
 					</tr>
 				</table>
 				
@@ -24,7 +24,7 @@
 				<table>
 					<tr>
 						<th><text>邮箱：</text></th>
-						<th><input type="text" v-model="user.mailbox"  maxlength="9" placeholder="邮箱" /></th>
+						<th><input type="text"  class="Register_input" v-model="user.mailbox"  maxlength="9" placeholder="邮箱" /></th>
 					</tr>
 					
 				</table>
@@ -40,7 +40,8 @@
 </template>
 
 <script>
-import * as api from '../../static/js/api.js' 
+	var	websock;
+	import * as api from '../../static/js/api.js' 
 
 	export default{
 		data(){
@@ -54,7 +55,7 @@ import * as api from '../../static/js/api.js'
 		},
 		methods:{
 			loginUser(){
-				uni.request({
+				/*uni.request({
 					url:api.registerUser,
 					data:{
 						'AccountName':this.user.AccountName,
@@ -66,9 +67,74 @@ import * as api from '../../static/js/api.js'
 					
 				})
 				console.log(this.user.AccountName);
-				console.log(this.user.UserPassWord)
-				//console.log(this.user.mailbox)
-			}
+				console.log(this.user.UserPassWord)*/
+				this.initWebSocket();
+			},
+			//创建WebSocket连接
+			initWebSocket() {
+				
+				const wsuri = "ws://127.0.0.1:4000";
+				websock = new WebSocket(wsuri);
+				websock.onopen = this.webSocketClientOnopen //打开
+				websock.onmessage = this.webSocketClientOnmessage //接收信息
+				websock.onerror = this.webSocketClientOnerror //错误
+				websock.onclose = this.webSocketClientOnclose //关闭
+			},
+			//打开连接
+			webSocketClientOnopen() {
+				console.log('打开成功')
+				var user = {
+					'AccountName': this.user.AccountName,
+					'UserPassWord': this.user.UserPassWord
+				};
+				var user1=JSON.stringify(user)
+				let actions = {
+					"Message": user1,
+					"Tag": "ac",
+					'ActionMethod': 'UserBLL.Register',
+				};
+				websock.send(JSON.stringify(actions));
+			},//数据回收
+			webSocketClientOnmessage(e) {
+						
+				var data = JSON.parse(e.data);
+				data= JSON.parse(data.Message);
+				
+				
+				if(data.Code==200){
+					//使用uni-app中的暂存 确保第二次免登
+					uni.setStorageSync('userInfo',JSON.stringify(data.Data))
+					//提示用户注册成功
+					uni.showToast({
+						//title: data.Data.AccountName + data.Message,
+						duration:1000
+					});
+					//延迟进入首页
+					setTimeout(()=>{
+						uni.reLaunch({
+							url: '../login/login'
+						})
+					},1001)
+				}else{
+					alert('请检查账号步骤是否正确！')
+				}
+			},
+			websocketsend(Data) { //发送数据
+				console.log('数据发送：' + JSON.stringify(Data));
+				websock.send(JSON.stringify(Data));
+			},
+			//重连信息
+			webSocketClientOnerror(e) {
+				this.initWebSocket();
+				console.log("websock连接错误,重新连接", e);
+			},
+			//连接关闭
+			webSocketClientOnclose(e) {
+				console.log("websock连接关闭", e);
+			},
+		},
+		created() {
+			
 		}
 	}
 	
